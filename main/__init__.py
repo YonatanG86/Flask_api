e912f6333f83a87ca1231360f0fec727cbce41ab
YonatanG <yonatangr86@gmail.com>
2021-05-08 21:38:05 +0300

    done

--- main/__init__.py
+++ main/__init__.py
@@ -8,7 +8,7 @@ from datetime import timedelta
 def create_app():
 
     app = Flask(__name__)
-    app.permanent_session_lifetime = timedelta(seconds=30)
+    app.permanent_session_lifetime = timedelta(seconds=60)
     app.config.from_object('config')
     mongo.init_app(app)
 
