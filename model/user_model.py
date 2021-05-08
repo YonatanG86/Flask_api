9b1b9482115f2b74cf1461f4401d7c6f8a3b6be0
YonatanG <yonatangr86@gmail.com>
2021-05-08 23:53:19 +0300

    update

--- model/user_model.py
+++ model/user_model.py
@@ -55,7 +55,7 @@ class User:
             "email": request.form.get('email')})
         if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
             return self.start_session(user)
-        return jsonify({"error": "Invalid loggin credentials"}), 401
+        return jsonify({"error": "Invalid login credentials"}), 401
 
     def all_users(self):
         users =mongo.db.users.find()
