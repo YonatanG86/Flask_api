b3d26890f8707056985c136a4f71afcba4b5b29e
YonatanG <yonatangr86@gmail.com>
2021-05-08 21:54:34 +0300

    admin added

--- model/message_model.py
+++ model/message_model.py
@@ -143,7 +143,7 @@ class Message:
         return jsonify({"messages": response}), 200
 
     
-    def all_messages_admin(self):
+    def all_users_admin(self):
         messages =mongo.db.messages.find()
         response =[]
         for message in messages:
